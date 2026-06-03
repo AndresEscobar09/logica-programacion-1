462b7c71aa89e26c6a23e993bf4f7080bc408a05
Andres Escobar <wilmera.escobarc@ecci.edu.co>
2026-06-03 15:04:38 -0500

    se realiza integracion de html y css se realizan pruebas

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-const prompt = require("prompt-sync")();
+//const prompt = require("prompt-sync")();
+
+let n1 = document.getElementById("numero1");
+let n2 = document.getElementById("numero2");
+let n3 = document.getElementById("numero3");
+
+const boton = document.getElementById("organizar");
+let texto = document.getElementById("numeros");
 
 //pruebas con consola
 
@@ -9,7 +16,6 @@ let numero2 = parseInt(prompt("ingrese el segundo número: "));
 let numero3 = parseInt(prompt("ingrese el tercer número: "));
 */
 
-let numeros = [numero1, numero2, numero3];
 
 
 function ordenarMayor(numeros){
@@ -34,3 +40,18 @@ if(numero1 === numero2 && numero2 === numero3){
     console.log("Los números ordenados de menor a mayor son: " + ordenarMenor(numeros));
     */
 }
+
+boton.addEventListener("click", function() {
+    let numero1 = parseInt(n1.value);
+    let numero2 = parseInt(n2.value);
+    let numero3 = parseInt(n3.value);
+    let numeros = [numero1, numero2, numero3];
+    
+
+    if(numero1 === numero2 && numero2 === numero3){
+        texto.innerText = "Los números son iguales";
+    }else{
+        texto.innerText = "De mayor a menor son: " + ordenarMayor(numeros) + "\n" + "De menor a mayor son: " + ordenarMenor(numeros);
+    }
+   
+});
